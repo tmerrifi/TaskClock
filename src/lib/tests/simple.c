fae36d09d3eef0f1c73ecdfe49d3a9678f28458e
tmerrifi <tmerri4@uic.edu>
2013-05-13 15:02:31 -0500

    new api so changing tests

--- src/lib/tests/simple.c
+++ src/lib/tests/simple.c
@@ -13,12 +13,9 @@ void do_work(int work_count){
 }
 
 int test1(){
-  struct determ_clock_info * clock = determ_clock_init();
-  determ_clock_start(clock);
+  determ_task_clock_start(0);
   do_work(5000);
-  determ_clock_read(clock);
-  printf("clock value is %d\n", clock->ticks);
-
+  printf("clock value is %d\n", determ_task_clock_read(0));
 }
 
 
