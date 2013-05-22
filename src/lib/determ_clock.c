f80cae2819a367288c79c473c32e39ce202cfb29
tmerrifi <tmerri4@uic.edu>
2013-05-22 11:30:24 -0500

    adding ioctl call

--- src/lib/determ_clock.c
+++ src/lib/determ_clock.c
@@ -124,7 +124,7 @@ void determ_task_clock_is_lowest_wait(){
 //lock is held when we enter
 void determ_task_clock_remove(){
   perf_counter_stop(task_clock_info.perf_counter);
-  if ( ioctl(pci->fd, PERF_EVENT_IOC_TASK_CLOCK_REMOVE, 0) != 0){
+  if ( ioctl(task_clock_info.perf_counter->fd, PERF_EVENT_IOC_TASK_CLOCK_REMOVE, 0) != 0){
     printf("\nreset wrong\n");
     exit(EXIT_FAILURE);
   }
