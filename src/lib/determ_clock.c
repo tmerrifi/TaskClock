5f020b1d5e14e7cc9df1372d66e0bbbb2bd91ba5
tmerrifi <tmerri4@uic.edu>
2013-05-22 18:07:16 -0500

    debugging

--- src/lib/determ_clock.c
+++ src/lib/determ_clock.c
@@ -96,9 +96,11 @@ void determ_task_clock_init(){
   //printf("HERE 2 ??????? tid %d pid %d cpu %d\n", task_clock_info.tid, getpid(), sched_getcpu());
   //set up the task clock for our process
   __make_clock_sys_call(task_clock_info.user_status, task_clock_info.tid, 0);
+  sleep(5);
   //set up the performance counter
 
   task_clock_info.perf_counter = perf_counter_init(DETERM_CLOCK_SAMPLE_PERIOD, (task_clock_info.tid==0) ? -1 : task_clock_info.perf_counter->fd );
+  printf("here %d\n", getpid());
 }
 
 
